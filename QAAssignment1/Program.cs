a71abfde224fd447fa9e1a1cf3521e71ef908fa4
Drashti Savaliya <dsavaliya1933@conestogac.on.ca>
2020-06-11 15:04:58 -0400

    update program.cs

--- QAAssignment1/Program.cs
+++ QAAssignment1/Program.cs
@@ -2,11 +2,147 @@
 
 namespace QAAssignment1
 {
-    class Program
+    public class Program
     {
+        public static void OptionBook()
+        {
+            Console.WriteLine("Choose suitable option :- ");
+            Console.WriteLine("========================================");
+
+            Console.WriteLine("1. Get Rectangle's length");
+            Console.WriteLine("2. Set Rectangle's length");
+            Console.WriteLine("3. Get Rectangle's width");
+            Console.WriteLine("4. Set Rectangle's width");
+            Console.WriteLine("5. Get Rectangle's perimeter");
+            Console.WriteLine("6. Get Rectangle's area");
+            Console.WriteLine("=====================");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("=====================");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rect = new Rectangle();
+            int selectedOption;
+
+            do
+            {
+                int length = 0;
+                Console.WriteLine("Enter Length:");
+                if (!Int32.TryParse(Console.ReadLine(), out length) || length <= 0)
+                {
+                    Console.WriteLine("Enter Valid Length");
+                    continue;
+                }
+                else
+                {
+                    rect.Length = length;
+                    break;
+                }
+            } while (true);
+            do
+            {
+                int width = 0;
+                Console.WriteLine("Enter Width:");
+                if (!Int32.TryParse(Console.ReadLine(), out width) || width <= 0)
+                {
+                    Console.WriteLine("Enter Valid Width");
+                    continue;
+                }
+                else
+                {
+                    rect.Width = width;
+                    break;
+                }
+            } while (true);
+
+            do
+            {
+                try
+                {
+                    OptionBook();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+                selectedOption = int.Parse(Console.ReadLine());
+
+                switch (selectedOption)
+                {
+                    case 0:
+
+                        Environment.Exit(0);
+
+                        break;
+
+                    case 1:
+
+                        int length = rect.GetLength();
+                        Console.WriteLine("\n\nLength = {0}", length);
+
+                        break;
+
+                    case 2:
+
+                        int newLength;
+                        Console.WriteLine("Current Length");
+                        newLength = int.Parse(Console.ReadLine());
+                        rect.Length = newLength;
+
+                        if (newLength > 0)
+                        {
+                            int finalLength = rect.SetLength(newLength);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter valid input");
+                        }
+                        break;
+
+                    case 3:
+
+                        int width = rect.GetWidth();
+                        Console.WriteLine("\n\nWidth = {0}", width);
+                        break;
+
+                    case 4:
+
+                        int newWidth;
+                        Console.WriteLine("Current Width");
+                        newWidth = int.Parse(Console.ReadLine());
+                        rect.Width = newWidth;
+
+                        if (newWidth > 0)
+                        {
+                            int finalWidth = rect.SetWidth(newWidth);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter valid input");
+                        }
+                        break;
+
+                    case 5:
+
+                        int finalPerimeter = rect.Getperimeter();
+                        Console.WriteLine("\n\nPerimeter = {0}", finalPerimeter);
+                        break;
+
+                    case 6:
+
+                        int finalArea = rect.GetArea();
+                        Console.WriteLine("\n\nArea = {0}", finalArea);
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Enter valid input");
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (true);
         }
     }
 }
--- QAAssignment1/Rectangle.cs
+++ QAAssignment1/Rectangle.cs
@@ -3,8 +3,79 @@ namespace QAAssignment1
 {
     public class Rectangle
     {
+        private int length;
+        private int width;
+
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                length = value;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+            }
+        }
+
         public Rectangle()
         {
+            Width = 1;
+            Length = 1;
+        }
+
+        public Rectangle(int Length1, int Width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        public int GetLength()
+        {
+            return this.Length;
+        }
+
+        public int SetLength(int l)
+        {
+            this.Length = l;
+            return this.Length;
+
+        }
+
+        public int GetWidth()
+        {
+            return this.Width;
+
+        }
+
+        public int SetWidth(int w)
+        {
+            this.Width = w;
+            return this.Width;
+        }
+
+        public int Getperimeter()
+        {
+            int perimeter = 2 * (this.Length + this.Width);
+            return perimeter;
+        }
+
+        public int GetArea()
+        {
+            int area = this.Length * this.Width;
+            return area;
         }
     }
 }
